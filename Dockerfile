c700487311bc4c0ec20a9478dd38e0ca3fe83ac3
Dominik Schröter <dschroter@bethel.jw.org>
2022-06-04 09:07:10 +0200

    Upgrade Meteor to 2.6.1

--- Dockerfile
+++ Dockerfile
@@ -5,11 +5,18 @@ RUN tar xvf /bundle/meteor.tar.gz
 
 FROM node:14
 COPY --from=prepare /tmp/bundle /bundle
-WORKDIR /bundle/programs/server/
 
 # renovate: datasource=npm depName=fibers
 ENV FIBERS_VERSION=4.0.3
-RUN npm install fibers@${FIBERS_VERSION} && npm install
+# renovate: datasource=npm depName=bcrypt
+ENV BCRYPT_VERSION=5.0.1
+
+WORKDIR /bundle/programs/server/npm/node_modules/meteor/accounts-password/
+RUN npm install bcrypt@${BCRYPT_VERSION}
+
+WORKDIR /bundle/programs/server/
+
+RUN npm install fibers@${FIBERS_VERSION} && npm install bcrypt@${BCRYPT_VERSION} && npm install
 WORKDIR /bundle
 CMD [ "node", "main.js" ]
 EXPOSE 8080
--- src/client/lib/clientStartup.ts
+++ src/client/lib/clientStartup.ts
@@ -1,52 +1,49 @@
 import * as underscore from "underscore";
 import * as s from "underscore.string";
 
-import {Meteor} from "meteor/meteor";
-import {Tracker} from "meteor/tracker";
-import {Accounts} from "meteor/accounts-base";
-import {Template} from "meteor/templating";
-import {Routes} from "../../lib/client/routes";
-
-declare var accountsUIBootstrap3:any;
+import { Meteor } from "meteor/meteor";
+import { Tracker } from "meteor/tracker";
+import { Accounts } from "meteor/accounts-base";
+import { Template } from "meteor/templating";
+import { Routes } from "../../lib/client/routes";
 
 Meteor.startup(function () {
 
 
-    Accounts.onLogout(function() {
-      FlowRouter.go("login"); // Wenn sich ein Benutzer ausgeloggt hat, wollen wir ihn zum Login schicken.
-    });
+  Accounts.onLogout(function () {
+    FlowRouter.go("login"); // Wenn sich ein Benutzer ausgeloggt hat, wollen wir ihn zum Login schicken.
+  });
 
 
-    /*
-     * #### Layout initialisieren ####
-     */
-    BlazeLayout.setRoot('body');
-    accountsUIBootstrap3.setLanguage('de'); // for German
-    bootbox.setLocale("de");
+  /*
+   * #### Layout initialisieren ####
+   */
+  BlazeLayout.setRoot('body');
+  bootbox.setLocale("de");
 
-    Accounts.onLogin(function() {
-      if(FlowRouter.getRouteName() === Routes.Def.Login.name) {
-        Routes.go(Routes.Def.Home);
-      }
-    });
+  Accounts.onLogin(function () {
+    if (FlowRouter.getRouteName() === Routes.Def.Login.name) {
+      Routes.go(Routes.Def.Home);
+    }
+  });
 });
 
 /*
  * #### Template-Helper für Underscore ####
  */
 
-Template.registerHelper('underscore', function(...args: Array<any>) {
-    args = underscore.toArray(args);
-    var self = this,
-        fn = arguments[0];
-    args.shift();
-    args.pop();
-    return underscore[fn].apply(self, arguments);
+Template.registerHelper('underscore', function (...args: Array<any>) {
+  args = underscore.toArray(args);
+  var self = this,
+    fn = arguments[0];
+  args.shift();
+  args.pop();
+  return underscore[fn].apply(self, arguments);
 });
 
 declare var Spacebars: any;
 
-Template.registerHelper('breaklines', function(text, options) {
+Template.registerHelper('breaklines', function (text, options) {
   text = s.escapeHTML(text);
   text = text.replace(/(\r\n|\n|\r)/gm, '<br/>');
   return new Spacebars.SafeString(text);
