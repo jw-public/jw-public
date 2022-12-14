e5e70eee2edca68f3ae45995b096d6a0fffa8240
Icereed <domi@icereed.net>
2022-12-14 14:35:29 +0100

    Fix copy assignments for 2023

--- src/client/templates/Group/CopyAssignments.ts
+++ src/client/templates/Group/CopyAssignments.ts
@@ -18,7 +18,7 @@ Template["copyAssignments"].events({
     const fromWeek = parseInt(event.target["fromWeek"].value, 10);
     const toWeek = parseInt(event.target["toWeek"].value, 10);
     const fromYear = moment().year()
-    const toYear = moment().year()
+    const toYear = 2023 // TODO: change after 01.01.2023
     console.log(fromWeek, toWeek, fromYear, toYear);
     let proxy = new ServerMethodsWrapper.GroupProxy(FlowRouter.getParam("groupId"));
 
@@ -39,7 +39,7 @@ Template["copyAssignments"].events({
         Alerts.add("Fehler beim kopieren: " + err.reason, "danger", {});
       } else {
         Alerts.removeSeen();
-        Alerts.add(`${totalCopied} Einsätze wurden von KW ${fromWeek} nach KW ${toWeek} kopiert.`, 'success',
+        Alerts.add(`${totalCopied} Einsätze wurden von KW ${fromWeek} ${fromYear} nach KW ${toWeek} ${toYear} kopiert.`, 'success',
           {
             fadeIn: 100, fadeOut: 100, autoHide: 30000
           });
