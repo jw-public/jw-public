9580c1fe424533544e10149a88630c39e2ce46e0
Dominik Schröter <dschroter@bethel.jw.org>
2023-01-21 00:40:10 +0100

    docs: Document DateDisplay component for more readable code
    This commit adds documentation to the DateDisplay component to make it more
    readable. This component is responsible for displaying start and end dates for
    an activity, and it uses moment.js for formatting these dates. The component
    interface and render method are now fully documented to make the component's
    purpose more clear.

--- src/client/react/components/DateDisplay.tsx
+++ src/client/react/components/DateDisplay.tsx
@@ -1,23 +1,29 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
 
 import * as moment from "moment";
 
-
+// Props interface for the DateDisplay component
 interface DateDisplayProps {
   start: Date;
   end: Date;
 }
+
+// DateDisplay component, using the React.Component class
+// and the DateDisplayProps interface as its props
 export default class DateDisplay extends React.Component<DateDisplayProps, {}> {
 
+  // Render method that returns JSX elements
   public render(): JSX.Element {
 
+    // moment.js is used to format the start and end dates
     const start = moment(this.props.start);
     const end = moment(this.props.end);
 
+    // Extract day of the month, month and time from the start date
     const dayOfMonth = start.format("Do");
     const month = start.format("MMM");
     const startTime = start.format("LT");
+    // Extract time from the end date
     const endTime = end.format("LT");
 
     return (
