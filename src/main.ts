2e03e26a358d055be07c96b71ea112cc5db0069b
Dominik Schröter <dschroter@bethel.jw.org>
2023-01-20 23:18:00 +0100

    Doc: Add documentation to the main.ts file
    This commit adds documentation to
    the main.ts file in the /src directory of
    the jw-public repository. The documentation
    consists of helper functions to check for
    empty strings, check if a user is an admin,
    and set initial values for error and success
    messages. Additionally, the commit adds logic
    to add or remove the "collapse" class in the
    div with class "navbar-collapse" depending
    on the width of the screen. This change will
    ensure that the code is more readable and
    easier to maintain going forward.

--- src/main.ts
+++ src/main.ts
@@ -4,24 +4,22 @@ import { Session } from "meteor/session";
 import { Template } from "meteor/templating";
 import ResizeSensor from "./lib/ResizeSensor";
 
-// Es folgt der Code, der Client-Seitig im Browser ausgeführt wird.
+// The following code runs client-side in the browser.
 if (Meteor.isClient) {
-
-    // Hilfsfunktion, um festzustellen, ob ein String leer ist.
+    // Helper function to check if a string is empty.
     Template.registerHelper('isEmptyString', function (value) {
-        // Hilfsfunktion, die feststellt, ob ein string leer ist.
-        // Diese wird im Spacebars-Template verwendet.
+        // Helper function that checks if a string is empty
+        // This is used in the Spacebars template
         return (!value || /^\s*$/.test(value));
     });
 
-
+    // Helper function to check if a user is an admin
     Template.registerHelper('isAdmin', function (user) {
         return Roles.userIsInRole(user, "admin");
     });
 
-
-
     Meteor.startup(function () {
+        // Set initial values for error and success messages
         Session.set('signUpErrorMessage', false);
         Session.set('signUpSuccessMessage', false);
         Session.set('loginErrorMessage', false);
@@ -30,18 +28,26 @@ if (Meteor.isClient) {
             var height, topOffset, width;
             topOffset = 50;
             width = (this.window.innerWidth > 0 ? this.window.innerWidth : this.screen.width);
+            // Check if the width of the screen is less than 768 pixels
             if (width < 768) {
+                // If it is, add the class "collapse" to the div with class "navbar-collapse"
                 $("div.navbar-collapse").addClass("collapse");
                 topOffset = 100;
             } else {
+                // If the width of the screen is greater than or equal to 768 pixels
+                // remove the class "collapse" from the div with class "navbar-collapse"
                 $("div.navbar-collapse").removeClass("collapse");
             }
             height = (this.window.innerHeight > 0 ? this.window.innerHeight : this.screen.height);
+            // Subtract the top offset from the height to get the final value
             height = height - topOffset;
+            // Ensure that the height is not less than 1
             if (height < 1) {
                 height = 1;
             }
+            // Check if the height is greater than the top offset
             if (height > topOffset) {
+                // If it is, set the min-height of the element with id "page-wrapper" to the calculated height
                 $("#page-wrapper").css("min-height", height + "px");
             }
         });
